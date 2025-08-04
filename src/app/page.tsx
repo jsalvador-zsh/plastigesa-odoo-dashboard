f74702e02b2ad64d6ba2e2a13e1d7f46b8318f41
jsalvador-zsh <juanstdev@gmail.com>
2025-08-03 23:18:03 -0500

    [ACT] Cambio en tamaño de imagenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,12 +50,12 @@ export default function Home() {
           <p className="text-lg text-foreground font-semibold">Tecnologías utilizadas</p>
         </div>
         <div className="flex flex-wrap items-center justify-center gap-10 px-6 animate-pulse-slow">
-          <Image src="/logos/nextjs.svg" alt="Next.js" width={40} height={20} style={{ height: 'auto', width: 'auto' }}/>
-          <Image src="/logos/odoo.svg" alt="Odoo" width={40} height={20} style={{ height: 'auto', width: 'auto' }} />
-          <Image src="/logos/clerk.svg" alt="Clerk" width={40} height={20} style={{ height: 'auto', width: 'auto' }} />
-          <Image src="/logos/shadcn.svg" alt="ShadCN" width={40} height={20} style={{ height: 'auto', width: 'auto' }} />
-          <Image src="/logos/tailwind.svg" alt="TailwindCSS" width={40} height={20} style={{ height: 'auto', width: 'auto' }} />
-          <Image src="/logos/vercel.svg" alt="Vercel" width={40} height={20} style={{ height: 'auto', width: 'auto' }} />
+          <Image src="/logos/nextjs.svg" alt="Next.js" width={20} height={20} style={{ height: '20', width: '40' }}/>
+          <Image src="/logos/odoo.svg" alt="Odoo" width={20} height={20} style={{ height: '20', width: '40' }} />
+          <Image src="/logos/clerk.svg" alt="Clerk" width={20} height={20} style={{ height: '20', width: '40' }} />
+          <Image src="/logos/shadcn.svg" alt="ShadCN" width={20} height={20} style={{ height: '20', width: '40' }} />
+          <Image src="/logos/tailwind.svg" alt="TailwindCSS" width={20} height={40} style={{ height: '20', width: '40' }} />
+          <Image src="/logos/vercel.svg" alt="Vercel" width={20} height={20} style={{ height: '20', width: '40' }} />
         </div>
       </section>
 
